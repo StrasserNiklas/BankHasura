80c75af89d6f1c26195d8d74ce66677d31bb02f9
StrasserNiklas <niklasstrasser@hotmail.com>
2022-03-25 00:11:27 +0100

    deleted angular proj

--- BankUI/BankUI/src/app/models/payment-transaction.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PaymentTransaction } from './payment-transaction';
-
-describe('PaymentTransaction', () => {
-  it('should create an instance', () => {
-    expect(new PaymentTransaction()).toBeTruthy();
-  });
-});
